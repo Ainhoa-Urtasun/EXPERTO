d85931e60e8314d16ced9b5fe3867105d18cf45d
Ainhoa <ainhoa.urtasun@unavarra.es>
2023-11-22 05:44:25 +0100

    Update EXPERTO0.py

--- EXPERTO0.py
+++ EXPERTO0.py
@@ -1,8 +1,8 @@
 from wordcloud import WordCloud, STOPWORDS
 import matplotlib.pyplot as plt
 
-def economia_social(es):
-    wc = WordCloud(background_color='black').generate_from_frequencies(es)
+def nube(diccionario):
+    wc = WordCloud(background_color='black').generate_from_frequencies(diccionario)
     fig = plt.figure(figsize = (9,6))
     plt.axis("off")
     return plt.imshow(wc)
