deda08f8929b01370f7df4fb1e7d7b49660c3080
Ainhoa <ainhoa.urtasun@unavarra.es>
2023-11-22 05:45:20 +0100

    Update EXPERTO0.py

--- EXPERTO0.py
+++ EXPERTO0.py
@@ -2,7 +2,7 @@ from wordcloud import WordCloud, STOPWORDS
 import matplotlib.pyplot as plt
 
 def nube(diccionario):
-    wc = WordCloud(background_color='black').generate_from_frequencies(diccionario)
+    wc = WordCloud(background_color='purple').generate_from_frequencies(diccionario)
     fig = plt.figure(figsize = (9,6))
     plt.axis("off")
     return plt.imshow(wc)
