f3faabffecd712e3d48398f4b013cd308a354529
Ainhoa <ainhoa.urtasun@unavarra.es>
2023-11-22 05:41:20 +0100

    Update EXPERTO0.py

--- EXPERTO0.py
+++ EXPERTO0.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 def economia_social(es):
     wc = WordCloud(background_color='black').generate_from_frequencies(es)
     fig = plt.figure(figsize = (9,6))
-    #plt.imshow(wc)
+    #plt.show()
     plt.axis("off")
-    return plt.show()
+    return plt.imshow(wc)
+    
