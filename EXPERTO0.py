47a58fb4cdd292bfb1ebc314d1a404526eb23cc9
Ainhoa <ainhoa.urtasun@unavarra.es>
2023-11-27 22:24:42 +0100

    Update EXPERTO0.py

--- EXPERTO0.py
+++ EXPERTO0.py
@@ -27,6 +27,8 @@ mydata = mydata[mydata['nace_r2']=='Industry, construction and market services (
 mydata = mydata[['geo',0]]
 mydata.rename(columns={'geo':'ADMIN'},inplace=True)
 mydata.rename(columns={0:'Number of firms'},inplace=True)
+tabla = mydata
+mydata = mydata[mydata['time']==2021]
 
 world = geopandas.read_file('/content/EXPERTO/ne_110m_admin_0_countries.zip')[['ADMIN','geometry']]
 polygon = Polygon([(-25,35),(40,35),(40,75),(-25,75)])
