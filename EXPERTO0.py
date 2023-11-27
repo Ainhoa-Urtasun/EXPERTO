5e986b12cd14b08d871a67aee883dc248df37c3c
Ainhoa <ainhoa.urtasun@unavarra.es>
2023-11-27 22:33:35 +0100

    Update EXPERTO0.py

--- EXPERTO0.py
+++ EXPERTO0.py
@@ -20,12 +20,12 @@ data = data.reindex(range(0,n),fill_value=0)
 structure = [pandas.DataFrame({key:val for key,val in metadata['dimension'][dim]['category'].items()}).sort_values('index')['label'].values for dim in metadata['id']]
 data.index = pandas.MultiIndex.from_product(structure,names=metadata['id'])
 mydata = data.reset_index()
+print(mydata)
 mydata = mydata[mydata['age']=='Total']
 mydata = mydata[mydata['sizeclas']=='Total']
 mydata = mydata[mydata['indic_sbs']=='Enterprises - number']
 mydata = mydata[mydata['nace_r2']=='Industry, construction and market services (except activities of membership organizations)']
 mydata = mydata[['geo',0]]
-print(mydata)
 mydata.rename(columns={'geo':'ADMIN'},inplace=True)
 mydata.rename(columns={0:'Número de empresas'},inplace=True)
 datos = mydata[mydata['time']==2021]
