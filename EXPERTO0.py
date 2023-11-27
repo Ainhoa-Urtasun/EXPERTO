6df6966dedac07c928e1df6a4110cb812e757bd0
Ainhoa <ainhoa.urtasun@unavarra.es>
2023-11-27 21:48:26 +0100

    Update EXPERTO0.py

--- EXPERTO0.py
+++ EXPERTO0.py
@@ -28,8 +28,6 @@ mydata = mydata[['geo',0]]
 mydata.rename(columns={'geo':'ADMIN'},inplace=True)
 mydata.rename(columns={0:'Number of firms'},inplace=True)
 mydata = mydata.pivot(index='ADMIN',columns='lev_satis',values='Thousand persons').reset_index()
-mydata['Porcentaje'] = 100*mydata['High']/mydata['Total']
-table = mydata[['ADMIN','Porcentaje']]
 
 world = geopandas.read_file('/content/EXPERTO/ne_110m_admin_0_countries.zip')[['ADMIN','geometry']]
 polygon = Polygon([(-25,35),(40,35),(40,75),(-25,75)])
