6b351e6226ca737a0ad11e834ff0e40a30f5f8d9
Ainhoa <ainhoa.urtasun@unavarra.es>
2023-11-28 10:23:33 +0100

    Update EXPERTO0.py

--- EXPERTO0.py
+++ EXPERTO0.py
@@ -23,8 +23,8 @@ mydata = data.reset_index()
 
 mydata = mydata[mydata['indic_sbs']=='Enterprises - number']
 mydata = mydata[mydata['nace_r2'].str.contains('Industry, construction and market services')]
-print(mydata)
 mydata = mydata[mydata['time']==2021]
+print(mydata)
 mydata = mydata[(mydata['leg_form']=='Sole proprietorship')|(mydata['leg_form']=='Partnership, co-operatives, associations, etc.')|(mydata['leg_form']=='Limited liability enterprise')]
 mydata = mydata[['geo','leg_form',0]]
 mydata.rename(columns={'geo':'ADMIN'},inplace=True)
